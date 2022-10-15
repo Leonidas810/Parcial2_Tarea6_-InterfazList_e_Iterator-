7c3cbb594b53f26181e7dc534e5a26a73e77f1de
Leonardo <89160879+Leonidas810@users.noreply.github.com>
2022-10-14 21:08:46 -0500

    first commit

---
+++ src/uaslp/objetos/list/linked/list/linkedlist/Node.java
@@ -0,0 +1,36 @@
+package uaslp.objetos.list.linked.list.linkedlist;
+
+public class Node {
+    private String data;
+    private Node next;
+    private Node previous;
+
+    //Constructor
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+}
